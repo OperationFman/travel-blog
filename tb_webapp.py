56036da00cb4710e59253cd6cd21792244c7593c
Franklin Moon <franklin.m.moon@gmail.com>
2021-01-06 15:21:26 +1100

    Removed debug mode

--- tb_webapp.py
+++ tb_webapp.py
@@ -40,6 +40,6 @@ def china():
     return render_template('china.html', page_title='Frankly China')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 """if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)"""
